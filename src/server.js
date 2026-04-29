4fd4ea0c72b7d76df9c987b6e47ef3ceb20a7e53
viitorfernandess <vitorazevedofer007@gmail.com>
2026-04-29 14:48:22 -0300

    feat: integra rotas ao servidor

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const router = require('./router')
 
 const app = express()
 
 app.use(express.json())
+
+app.use("/api", router)
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Servidor rodandno em http://localhost:${PORT}`))
