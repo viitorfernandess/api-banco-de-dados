20f32dbe762b6939cc9df758f9f0ba7f2f249b62
viitorfernandess <vitorazevedofer007@gmail.com>
2026-04-29 15:42:24 -0300

    chore: remove .env do versionamento

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,9 @@
 require('dotenv').config()
-console.log('ENV:', process.env.DATABASE_URL)
 const { Pool } = require('pg')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
 
-
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-        console.log('Erro:', err)
-    } else {
-        console.log('Conectado', res.rows)
-    }
-})
 
 module.exports = pool
