6c1f280ad2f5cf4d38fdb4b3e0d7a7ce4e607c54
viitorfernandess <vitorazevedofer007@gmail.com>
2026-04-29 16:37:42 -0300

    feat: centraliza acesso ao banco com função query

--- src/database/index.js
+++ src/database/index.js
@@ -5,5 +5,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
 
+async function query(queryString, params, callback) {
+    return pool.query(queryString, params, callback)
+}
 
-module.exports = pool
+module.exports = { query }
