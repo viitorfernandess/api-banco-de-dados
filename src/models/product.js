b3f6284ef7fc45a515c16dc0e869f3efffafbac9
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-07 15:53:18 -0300

    feat: adiciona método de criação de produtos

--- src/models/product.js
+++ src/models/product.js
@@ -13,8 +13,16 @@ class Product {
     }
 
     static async findAll() {
-        const result = await query( `SELECT * FROM products;`)
+        const result = await query (`SELECT * FROM products;`)
         return result.rows.map((row) => new Product(row))
+    }
+
+    static async create({ name, description, price, stockQuantity, isActive }) {
+        await query (
+            `INSERT INTO products (name, description, price, stock_quantity, is_active)
+            VALUES ($1, $2, $3, $4, $5)`,
+            [name, description, price, stockQuantity, isActive]
+        )
     }
 }
 
