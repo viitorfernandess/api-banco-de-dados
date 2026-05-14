35aadcae079f89391198e4346ce6b86b637a27a7
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-14 10:50:35 -0300

    feat: adiciona tabela customers

--- src/database/sync-database.js
+++ src/database/sync-database.js
@@ -13,8 +13,20 @@ async function syncDatabase() {
         is_active BOOLEAN DEFAULT TRUE
         );
         `);
-        console.log('Created "products" table.');
-        process.exit(1);
+    console.log('Created "products" table.');
+
+    await query(`
+         CREATE TABLE IF NOT EXISTS customers (
+         id SERIAL PRIMARY KEY,
+         name VARCHAR(255) NOT NULL,
+         email VARCHAR(255) NOT NULL UNIQUE,
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+         updated_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+         );   
+     `)
+    console.log('Created "customers" table.');
+
+    process.exit(1);
 }
 
 syncDatabase()
