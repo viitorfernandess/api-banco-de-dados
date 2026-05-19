0316f6222d29e0c85dc7fdd5558c5bfe67a7ff6a
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-19 15:41:40 -0300

    feat: adiciona método create no controller de customers

--- src/controllers/customers-controller.js
+++ src/controllers/customers-controller.js
@@ -7,5 +7,10 @@ const customerController = {
         return res.json(customers)
     },
 
-    
+    // POST /customers
+    create: async (req, res) => {
+        const newCustomer = await Customer.create(req.body)
+        res.status(201).json(newCustomer)
+    },
+
 }
