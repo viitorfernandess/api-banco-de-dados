614f17106b1a9526ad93034c6ec31c24951bea6f
viitorfernandess <vitorazevedofer007@gmail.com>
2026-05-27 10:58:07 -0300

    feat: adiciona busca de pedidos com dados do cliente

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,7 +1,7 @@
 class Order {
     constructor(orderRow, populateCustomer, populateProducts) {
         this.id = orderRow.id
-        this.customerId = orderRow.customerId
+        this.customerId = orderRow.customer_id
         this.total = +orderRow.total
         this.createdAt = new Date(orderRow.created_at)
         this.updatedAt = new Date(orderRow.updated_at)
@@ -11,8 +11,21 @@ class Order {
             this.customer = populateCustomer
         }
 
-        if (populateProduct) {
+        if (populateProducts) {
             this.products = populateProducts
         }
+    }
+
+    static async findAll() {
+        const result = await query(`
+    SELECT
+        orders.*,
+        customers.id AS "customer.id",
+        customers.name AS "customer.name",
+        cusomers.email AS "customer.email",
+        customers.created_at AS "customer.created_at",
+        customers.updated_at AS "custoemrs.updated_at"
+    FROM orders JOIN customers ON customers.id = orders.customer_id;     
+    `)
     }
 }
